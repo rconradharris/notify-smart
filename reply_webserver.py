905096990b5fdc8132dd48f14c9bdf42409c590a
Rick Harris <rconradharris@gmail.com>
2016-10-27 01:18:46 +0000

    Adding secret for rudimentary security

--- reply_webserver.py
+++ reply_webserver.py
@@ -2,6 +2,7 @@
 """
 Webserver to serve the reply form.
 """
+import ConfigParser
 import os
 import time
 
@@ -14,9 +15,32 @@ REPLY_WAIT = 0.5
 REPLY_DIRECTORY = os.path.expanduser('~/.irssi/reply-data')
 TRANSCRIPTS_DIRECTORY = os.path.expanduser('~/.irssi/transcripts')
 
+CONFIG_PATH = os.path.expanduser('~/.irssi')
+CONFIG_FILE = os.path.join(CONFIG_PATH, 'server-irc-notifier.cfg')
+
+CFG = None
+
+def _validate_secret(secret):
+    global CFG
+    if not CFG:
+        if not os.path.exists(CONFIG_FILE):
+            print "error: required config file not found at '{}'".format(CONFIG_FILE)
+            return False
+        CFG = ConfigParser.ConfigParser()
+        CFG.read(CONFIG_FILE)
+    try:
+        cfg_secret = CFG.get('reply', 'secret')
+    except ConfigParser.NoOptionError:
+        print "Must supply secret in config file at '{}'".format(CONFIG_FILE)
+        return False
+    return secret == cfg_secret
+
 
 @app.route('/reply/<target>', methods=['GET', 'POST'])
 def reply(target):
+    secret = flask.request.args.get('secret', '')
+    if not _validate_secret(secret):
+        return flask.abort(404)
     if flask.request.method == 'POST':
         # Write our reply to reply-data directory which is the communication
         # channel between the webserver and the reply.pl irssi plugin
@@ -30,7 +54,8 @@ def reply(target):
         # Give the reply.pl poller a chance to actually emit the new message
         time.sleep(REPLY_WAIT)
 
-        return flask.redirect(flask.url_for('reply', target=target))
+        return flask.redirect(
+            flask.url_for('reply', target=target, secret=secret))
     else:
         path = os.path.join(TRANSCRIPTS_DIRECTORY, target)
         if not os.path.exists(path):
